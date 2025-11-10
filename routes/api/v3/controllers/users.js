799fb1ad43d6730af23d9e73e6b87d71927bb8e0
TeresaWww <teresa9_5@163.com>
2025-11-10 14:08:15 -0800

    updated auth

--- routes/api/v3/controllers/users.js
+++ routes/api/v3/controllers/users.js
@@ -14,36 +14,10 @@ router.get('/myIdentity', function(req, res) {
     return res.json({
         "status": "loggedin",
         "userInfo":{
-            "name": "Teresa Wang",
-            "username": "twang85@uw.edu"
+            "name": session.account.name,
+            "username": session.account.username
         }
     })
 });
 
 export default router;
-
-
-// router.post('/signin', (req, res) => {
-
-//   if(req.session.username){
-//     res.send("Error: you are already logged in as " + req.session.userid )
-//     return
-//   }
-
-//   // check username and password
-//   if(req.body.username == "teresa" && req.body.password == "asdasd"){
-//     req.session.userid = "kylethayer"
-//     res.send("you logged in")
-//   } else if(req.body.username == "anotheruser" && req.body.password == "pwd"){
-//     req.session.userid = "anotheruser"
-//     res.send("you logged in")
-//   } else {
-//     res.send("wrong login info")
-//   }
-// })
-
-// router.post('/logout', (req, res) => {
-//   req.session.destroy()
-//   res.send("you are logged out")
-// })
-
